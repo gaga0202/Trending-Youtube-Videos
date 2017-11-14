6c4acfda367f286c13a314d920e5507796786a88
gaga0202 <gagannain0202@gmail.com>
2017-11-14 20:47:23 +1100

    added helmet and added version for frontend scripts

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
     path    = require('path'),
+    helmet  = require('helmet'),
+    version = require('./package.json').version,
     port    = 3002;
 
 var app = express();
@@ -8,6 +10,9 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+// Don't cache the remaining non-static things
+app.use(helmet.noCache());
+
 app.listen(port, function () {
   console.log('Server started at ' + port);
 });
@@ -15,3 +20,7 @@ app.listen(port, function () {
 app.get('/api/test', function (req, res) {
   res.status(200).json({message: 'Hello the app is working'});
 });
+
+app.get('*', function (req, res) {
+  res.locals.version = version;
+})
