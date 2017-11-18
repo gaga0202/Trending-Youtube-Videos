cfaf8a095da851c5cbb2d369b6d01b0616efb908
gaga0202 <gagannain0202@gmail.com>
2017-11-18 20:01:01 +1100

    made templates module and server routes for templates

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,14 @@
+var Templates = require('./templates.server');
+
 module.exports = function (app) {
+
+  /**
+   * ======================= Templates =========================================
+   */
+    app.get('/html-templates/home', Templates.home);
+   /**
+    * ======================= TEST ROUTE =======================================
+    */
   app.get('/api/test', function (req, res) {
     res.status(200).json({message: 'Hello the app is working'});
   });
