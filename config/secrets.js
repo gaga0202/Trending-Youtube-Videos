8193803978079ea88dd3c5dcdfd4d21713b69945
gaga0202 <gagannain0202@gmail.com>
2017-11-21 20:28:11 +1100

    added youtube api key

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,5 @@
 module.exports = {
-  db:         process.env.MONGODB_ADDRESS,
-  localDb:    process.env.MONGODB_LOCAL
+  db:             process.env.MONGODB_ADDRESS,
+  localDb:        process.env.MONGODB_LOCAL,
+  youtubeApiKey:  process.env.YOUTUBE_API_KEY,
 };
