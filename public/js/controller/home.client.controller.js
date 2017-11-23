92c0205fddaeb007a1692885f22ea2e6021e9052
gaga0202 <gagannain0202@gmail.com>
2017-11-23 20:01:16 +1100

    home page to work

--- public/js/controller/home.client.controller.js
+++ public/js/controller/home.client.controller.js
@@ -1,34 +1,47 @@
-app.controller('HomeController',['$scope', '$http', 'toastr', '$routeParams',
+app.controller('HomeController',['$scope', '$location', 'toastr', '$routeParams',
 'CountryService', 'TrendService', 
-function ($scope, $http, toastr, $routeParams, CountryService, TrendService) {
+function ($scope, $location, toastr, $routeParams, CountryService, TrendService) {
   // ----------------------- Controller variables ----------------------------
   var countryCode = $routeParams.code;
 
   // -------------------------- Scope variables ------------------------------
   $scope.country = {};
-
+  $scope.getTrendForCountry = getTrendForCountry;
   // -------------------------- Scope functions ------------------------------
   // $scope.addCountry = addCountry;
 
   // ----------------------- Function Declaration ----------------------------
   function initialize() {
-    var pCountryList = CountryService.getAll();
-    var pTrendingList;
+    CountryService.getAll()
+      .then(function (result) {
+        $scope.country.countries = result.data.countries;
+      })
+      .catch(function (error) {
+        toastr.error(error.data.message, {timeOut: 1500});
+      })
+
+    var pTrendingList; 
     if (countryCode) {
       pTrendingList = TrendService.getTrend(countryCode);
     } else {
       pTrendingList = TrendService.getTrend('AU');
     }
-    Promise.all([pCountryList, pTrendingList])
-      .then(function (resultArray) {
-        $scope.countries = resultArray[0].data.countries;
-        $scope.trendingVideos = resultArray[1].data.trendingVideos;
+    pTrendingList
+      .then(function (result) {
+        $scope.trendingVideos = result.data.trendingVideos;
+        $scope.country.countryName = result.data.countryName;
+        $scope.country.countryCode = result.data.countryCode;
+        var code = angular.element(document.getElementById('image'));
+        console.log(code);
       })
-      .catch(function (errorArray) {
-        console.log(errorArray);
+      .catch(function (error) {
+        toastr.error(errorArray.data.message, {timeOut: 1500});
       });
   }
 
+  function getTrendForCountry() {
+    $location.url('/?code=' + $scope.country.code);
+  }
   // ----------------------- Controller Operations ----------------------------
   initialize();
 }]);
