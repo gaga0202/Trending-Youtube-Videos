cb6d9593ffb3b9afbb5248e7bfe32aae5cee6c98
gaga0202 <gagannain0202@gmail.com>
2017-11-24 19:11:30 +1100

    watch-video page changes

--- public/js/controller/watch-video.client.controller.js
+++ public/js/controller/watch-video.client.controller.js
@@ -1,6 +1,6 @@
 app.controller('WatchVideoController',[
-'$scope', '$location', 'TrendService', '$routeParams',
-function ($scope, $location, TrendService, $routeParams) {
+'$scope', '$location', 'TrendService', '$routeParams', 'VideoPlayer', 'toastr',
+function ($scope, $location, TrendService, $routeParams, VideoPlayer, toastr) {
   
   //===================== Variable Initialization ==============================
   var page = 1;
@@ -22,8 +22,15 @@ function ($scope, $location, TrendService, $routeParams) {
     TrendService.getVideoDetails($scope.videoId)
       .then(function (result) {
         $scope.video = result.data.video;
+        console.log($scope.video);
+        var externalSource = {
+          source:     'youtube',
+          url:        'www.youtube.com/watch?v=' + $scope.video.videoId
+        };
+        var player = VideoPlayer.videoJSPlayer('main-video', externalSource);
       })
       .catch(function (error) {
+        console.log(error);
         toastr.error(error.data.message, {timeout: 1500});
         if (error.status === 404) {
           $location.url('/');
@@ -38,7 +45,8 @@ function ($scope, $location, TrendService, $routeParams) {
     };
     TrendService.watchMore(details)
       .then(function (result) {
-        console.log(result);
+        console.log(result.data);
+        $scope.videos = result.data.videos;
       })
       .catch(function (error) {
         console.log(error);
