7ce09f44d8c672b38fd266ddf6cc3b35054b51dc
gaga0202 <gagannain0202@gmail.com>
2017-11-26 10:49:42 +1100

    removed console.log

--- public/js/controller/home.client.controller.js
+++ public/js/controller/home.client.controller.js
@@ -20,7 +20,6 @@ function ($scope, $location, toastr, $routeParams, CountryService, TrendService)
     }
     pTrendingList
       .then(function (result) {
-        console.log(result);
         $scope.trendingVideos = result.data.trendingVideos;
         $scope.country.countryName = result.data.countryName;
         $scope.country.countryCode = result.data.countryCode;
